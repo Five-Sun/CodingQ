e33503695767a540e1c2beda3c3b0f255d67cc95
Oh-Tae-Yang <87643153+Five-Sun@users.noreply.github.com>
2025-11-21 16:25:48 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 25604 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            if (Math.abs(a) == Math.abs(b)) return a.compareTo(b);
+            return Math.abs(a) - Math.abs(b);
+        });
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            switch (num) {
+                case 0:
+                    if (pq.isEmpty()) {
+                        sb.append(0).append("\n");
+                    } else {
+                        sb.append(pq.poll()).append("\n");
+                    }
+                    break;
+                default:
+                    pq.offer(num);
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
