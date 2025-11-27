6f4effac77f1b9b77607363a7e9760cf9b05cc32
Oh-Tae-Yang <87643153+Five-Sun@users.noreply.github.com>
2025-11-27 20:25:49 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14908 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,34 +1,40 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    static int [] arr;
-    static Integer [] dp;
-    static int N;
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
 
-        N = sc.nextInt();
-        arr = new int [N + 1];
-        dp = new Integer [N + 1];
-
+        int N = Integer.parseInt(st.nextToken());
+        int[] wine = new int[N + 1];
 
         for (int i = 1; i <= N; i++) {
-            arr[i] = sc.nextInt();
+            wine[i] = Integer.parseInt(br.readLine());
         }
 
-        dp[0] = 0;
-        dp[1] = arr[1];
+        if (N == 1) {
+            System.out.println(wine[1]);
+            return;
+        }
+        if (N == 2) {
+            System.out.println(wine[1] + wine[2]);
+            return;
+        }
 
-        if (N > 1) {
-            dp[2] = arr[1] + arr[2];
+        int[] dp = new int[N + 1];
+
+        dp[1] = wine[1];
+        dp[2] = wine[1] + wine[2];
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wine[i], dp[i - 3] + wine[i - 1] + wine[i]));
         }
-        
-        System.out.println(recur(N));
-    }
-    public static int recur(int N) {
-        if (dp[N] == null) {
-            dp[N] = Math.max(Math.max(recur(N - 2), recur(N-3) + arr[N-1])  + arr[N], recur(N-1));
-        }
-        return dp[N];
+        System.out.println(dp[N]);
     }
 }
+//case 1: i번째꺼 안마시면: dp[i-1]
+//case 2: i번째꺼 먹고, 전꺼 안먹음: dp[i] = dp[i-2] + wine[i]
+//case 3: i번째꺼 먹고, 전꺼 먹음: dp[i] = dp[i-3] + wine[i-1] + wine[i]
