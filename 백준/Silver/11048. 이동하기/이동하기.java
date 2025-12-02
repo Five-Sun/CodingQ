2ca97b9870b696e84b57cc84ca29c708ff78f0b3
Oh-Tae-Yang <87643153+Five-Sun@users.noreply.github.com>
2025-12-02 20:42:46 +0900

    [Silver II] Title: 이동하기, Time: 492 ms, Memory: 76428 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[N + 1][M + 1];
+        for (int r = 1; r <= N; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 1; c <= M; c++) {
+                map[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] dp = new int[N + 1][M + 1];
+        for (int r = 1; r <= N; r++) {
+            for (int c = 1; c <= M; c++) {
+                dp[r][c] = Math.max(dp[r - 1][c], Math.max(dp[r][c - 1], dp[r - 1][c - 1])) + map[r][c];
+            }
+        }
+        System.out.println(dp[N][M]);
+    }
+}
+//위, 대각선 위, 옆에서 올 수 있음
