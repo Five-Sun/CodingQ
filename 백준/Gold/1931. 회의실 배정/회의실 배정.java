2c5c1cff97030b1a9d28784ca5b1903d9369ec1e
Oh-Tae-Yang <87643153+Five-Sun@users.noreply.github.com>
2025-12-09 11:47:59 +0900

    [Gold V] Title: 회의실 배정, Time: 468 ms, Memory: 44040 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
+            } else {
+                return a[1] - b[1];
+            }
+        });
+
+        int lastTime = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            int start = arr[i][0];
+            int finish = arr[i][1];
+
+            if (lastTime <= start) {
+                lastTime = finish;
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
