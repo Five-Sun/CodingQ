7c0afa5c56c18f25b24b92f08f1264d1fa2c8f60
Oh-Tae-Yang <87643153+Five-Sun@users.noreply.github.com>
2026-03-16 00:19:09 +0900

    [Gold V] Title: 최소비용 구하기, Time: 480 ms, Memory: 52308 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,70 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int N, M;
+    static List<Node>[] graph;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
 
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
 
-        List<List<Node>> list = new ArrayList<>();
-        for (int i = 0; i < N + 1; i++) {
-            list.add(new ArrayList<>());
+        graph = new ArrayList[N + 1];
+
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
         }
-        for (int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int node1 = Integer.parseInt(st.nextToken());
-            int node2 = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-            list.get(node1).add(new Node(node2, cost));
+            graph[u].add(new Node(v, w));
         }
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> {
-            return a.cost - b.cost;
-        });
+        int answer = djikstra(start, end);
 
+        System.out.println(answer);
+    }
+
+    static int djikstra(int start, int end) {
         int[] dist = new int[N + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        int INF = Integer.MAX_VALUE;
+        for (int i = 0; i <= N; i++) {
+            dist[i] = INF;
+        }
+        dist[start] = 0;
 
-        dist[A] = 0;
-        pq.add(new Node(A, 0));
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
 
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
-            int curPoint = cur.point;
-            int curCost = cur.cost;
+            int u = cur.v;
+            int w = cur.w;
 
-            if (curCost > dist[curPoint]) {
-                continue;
-            }
+            if (dist[u] < w) continue;
 
-            for (Node neighbor : list.get(curPoint)) {
-                int newDist = curCost + neighbor.cost;
+            for (Node node : graph[u]) {
+                int next = node.v;
+                int nextW = node.w;
+                int newDist = dist[u] + nextW;
 
-                if (newDist < dist[neighbor.point]) {
-                    dist[neighbor.point] = newDist;
-                    pq.add(new Node(neighbor.point, newDist));
+                if (dist[next] > newDist) {
+                    dist[next] = newDist;
+                    pq.offer(new Node(next, newDist));
                 }
             }
         }
-        System.out.println(dist[B]);
+
+        return dist[end];
     }
 
-    static class Node {
-        int point;
-        int cost;
+    static class Node implements Comparable<Node>{
+        int v, w;
 
-        public Node(int point, int cost) {
-            this.point = point;
-            this.cost = cost;
+        Node(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.w - o.w;
         }
     }
 }
