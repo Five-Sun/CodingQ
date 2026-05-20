c81707326ef8b3b6aa007e1acdd72088f6e65e4e
Oh-Tae-Yang <87643153+Five-Sun@users.noreply.github.com>
2026-05-20 14:24:32 +0900

    [level 2] Title: 시소 짝꿍, Time: 5710.73 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,32 +1,32 @@
-import java.util.*;
-
+import java.util.Arrays;
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
+
         Arrays.sort(weights);
 
-        int len = weights.length;
-        int count = 0;
-        int before = 0;
-        for (int i = 0; i < weights.length - 1; i++) {
-            if (before != weights[i]) {
-                before = weights[i];
-                count = 0;
-                for (int j = i + 1; j < weights.length; j++) {
-                    double div = (double) weights[i] / weights[j];
-                    if (div == (double) 1 /2 || div == (double) 2 /3 || div == (double) 3 /4 || div == 1.0) count++;
+        for(int i = 0; i < weights.length-1; i++){
+            for(int j = i + 1; j < weights.length; j++){
+                if(weights[i]*2 < weights[j]) break;
+                if(weights[i] == weights[j]) {
+                    answer++;
+                    continue;
                 }
-                answer += count;
-            } else {
-                count -= 1;
-                answer += count;
+                if(weights[i]*3 == weights[j]*2) {
+                    answer++;
+                    continue;
+                }
+                if(weights[i]*4 == weights[j]*2) {
+                    answer++;
+                    continue;
+                }
+                if(weights[i]*4 == weights[j]*3) {
+                    answer++;
+                    continue;
+                }
             }
-
         }
 
         return answer;
     }
 }
-/**
- * 정렬 -> while()
- */
