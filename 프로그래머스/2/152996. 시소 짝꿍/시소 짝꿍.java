74416d21e64b4b485ca35778af4336e7cc729bf3
Oh-Tae-Yang <87643153+Five-Sun@users.noreply.github.com>
2026-05-20 14:22:23 +0900

    [level 2] Title: 시소 짝꿍, Time: 229.45 ms, Memory: 65.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+        Arrays.sort(weights);
+
+        int len = weights.length;
+        int count = 0;
+        int before = 0;
+        for (int i = 0; i < weights.length - 1; i++) {
+            if (before != weights[i]) {
+                before = weights[i];
+                count = 0;
+                for (int j = i + 1; j < weights.length; j++) {
+                    double div = (double) weights[i] / weights[j];
+                    if (div == (double) 1 /2 || div == (double) 2 /3 || div == (double) 3 /4 || div == 1.0) count++;
+                }
+                answer += count;
+            } else {
+                count -= 1;
+                answer += count;
+            }
+
+        }
+
+        return answer;
+    }
+}
+/**
+ * 정렬 -> while()
+ */
